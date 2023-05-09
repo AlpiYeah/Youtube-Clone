82e6652c3c23ee86acb0a7d1a8549d341166fc73
Alp <alpersyuleyman2002@gmail.com>
2023-05-09 13:11:47 +0200

    populated the sidebar with all categories

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,6 +1,7 @@
 import {useState, useEffect} from 'react'
 import { Box, Stack, Typography } from '@mui/material'
-import { Sidebar } from './Sidebar'
+import Sidebar from './Sidebar'
+
 
 
 const Feed = () => {
