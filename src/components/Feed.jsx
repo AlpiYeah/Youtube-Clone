68f9e33754d73b2f0cb1e0bb05381de5db6524bf
Alp <alpersyuleyman2002@gmail.com>
2023-05-10 13:41:31 +0200

    succesfull fetch of videos for each category

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -7,11 +7,20 @@ import { fetchFromAPI } from '../utils/fetchFromAPI'
 
 const Feed = () => {
 
-  
+  const [selectedCategory, setselectedCategory] = useState("New")
+  const [videos, setvideos] = useState([])
+
+  useEffect(() => {
+fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then((data)=>setvideos(data.items))
+  }, [selectedCategory])
+
   return (
     <Stack sx={{ flexDirection: { sx: "column", md:"row"}}}>
       <Box sx={{ height: { sx:"auto", md: "92vh"}, borderRight:"1px solid #3d3d3d", px:{ sx:0, md: 2}}}>
-<Sidebar/>
+<Sidebar
+selectedCategory={selectedCategory}
+setselectedCategory={setselectedCategory}
+/>
 
 <Typography className='copyright' variant='body2' sx={{mt: 1.5, color:"#fff"}}>
   Copyright 2023 Brasco Media
@@ -28,9 +37,9 @@ const Feed = () => {
   sx={{
     color:"white"
   }}>
-    New <span style={{ color: "#F31503"}}>videos</span>
+    {selectedCategory} <span style={{ color: "#F31503"}}>videos</span>
   </Typography>
-  <Videos/>
+  <Videos videos={videos}/>
 </Box>
     </Stack>
   )
--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -4,7 +4,9 @@ const BASE_URL = "https://youtube-v31.p.rapidapi.com";
 
 const options = {
   url: BASE_URL,
-  params: {},
+  params: {
+    maxResults: 30,
+  },
   headers: {
     "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
     "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
