6d2f5ffb5d1fdb5a957ec076f659e9f505e5da9a
Alp <alpersyuleyman2002@gmail.com>
2023-05-10 19:08:08 +0200

    removed demo channel and video stuff

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -2,14 +2,12 @@ import { Box, CardContent, CardMedia, Typography } from "@mui/material"
 import { CheckCircle } from "@mui/icons-material"
 import {Link} from "@mui/material"
 
-import { demoProfilePicture } from "../utils/constants"
-
 const ChannelCard = ( {channelDetail}) => (
   <Box sx={{ boxShadow:"none", borderRadius: "20px", display:"flex", justifyContent:"center", alignItems:"center", width:{ xs:"356px", md:"320px"}, height:"326px", margin:"auto"}}>
 <Link to={`/channel/${channelDetail?.id?.channelId}`}>
 <CardContent sx={{ display: "flex", flexDirection: "column", justifyContent:"center", textAlign: "center", color:"#fff"}}>
 <CardMedia 
-image={channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture}
+image={channelDetail?.snippet?.thumbnails?.high?.url}
 alt={channelDetail?.snippet?.title}
 sx={{ borderRadius: "50%", height:"180px", width:"180px", mb: 2, border: "1px solid #e3e3e3"}}
 />
--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -2,12 +2,10 @@ import { Link } from "react-router-dom"
 import { Typography, Card, CardContent, CardMedia } from "@mui/material"
 import { CheckCircle } from "@mui/icons-material"
 
-import { demoThumbnailUrl, demoVideoUrl, demoChannelTitle, demoChannelUrl, demoVideoTitle } from "../utils/constants"
-
 const VideoCard = ({video: { id: {videoId}, snippet}}) => {
   return (
     <Card sx={{ width: {md:"320px", xs:"100%"}, boxShadow:"none", borderRadius:0}}>
-        <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+        <Link to={`/video/${videoId}`}>
         <CardMedia 
         image={snippet?.thumbnails?.high?.url}
         alt={snippet?.title}
@@ -15,12 +13,12 @@ const VideoCard = ({video: { id: {videoId}, snippet}}) => {
         />
         </Link>
 <CardContent sx={{ backgroundColor: "#1e1e1e", height: "100px"}}>
-<Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+<Link to={`/video/${videoId}`}>
     <Typography variant="subtitle1" fontWeight="bold" color="#FFF">
         {snippet?.title.slice(0,60)}
     </Typography>
     </Link>
-    <Link to={snippet?.channelId ? `/channel/${snippet?.channelId}` : demoChannelUrl}>
+    <Link to={`/channel/${snippet?.channelId}`}>
     <Typography variant="subtitle2" fontWeight="bold" color="gray">
         {snippet?.channelTitle.slice(0,60)}
         <CheckCircle sx={{ fontSize: 14, color:"gray", ml: "5px"}}/>
