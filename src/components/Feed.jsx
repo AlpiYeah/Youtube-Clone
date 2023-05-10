6503af90d119cd557707be0f1cc2916fd3b38e20
Alp <alpersyuleyman2002@gmail.com>
2023-05-10 13:10:32 +0200

    set up teh api call

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,10 +1,13 @@
 import {useState, useEffect} from 'react'
 import { Box, Stack, Typography } from '@mui/material'
 import {Sidebar, Videos } from "./"
+import { fetchFromAPI } from '../utils/fetchFromAPI'
 
 
 
 const Feed = () => {
+
+  
   return (
     <Stack sx={{ flexDirection: { sx: "column", md:"row"}}}>
       <Box sx={{ height: { sx:"auto", md: "92vh"}, borderRight:"1px solid #3d3d3d", px:{ sx:0, md: 2}}}>
