a52bd036528a53ca2aefeacdef95d7a49cb043e7
Alp <alpersyuleyman2002@gmail.com>
2023-05-26 18:48:53 +0200

    working on displaying channel PFP on video cards

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -2,9 +2,25 @@ import { Link } from "react-router-dom"
 import { Typography, Card, CardContent, CardMedia } from "@mui/material"
 import { CheckCircle } from "@mui/icons-material"
 import { motion } from "framer-motion"
-import Avatar from "@mui/material"
+import {Avatar} from "@mui/material"
+import { Stack } from "@mui/system"
+import { useState, useEffect } from "react"
+import { fetchFromAPI } from "../utils/fetchFromAPI"
 
-const VideoCard = ({video: { id: {videoId}, snippet}}, {channelDetail}) => {
+const VideoCard = ({video: { id: {videoId}, snippet}}) => {
+
+  const [channelDetail, setChannelDetail] = useState(null)
+
+  useEffect(() => {
+    const fetchResults = async () => {
+      const data = await fetchFromAPI(`channels?id=${snippet?.channelId}`);
+
+      setChannelDetail(data?.items[0]); };
+
+      fetchResults();
+    },);
+  
+
   return (
 
     <motion.div whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }} >
@@ -25,12 +41,16 @@ const VideoCard = ({video: { id: {videoId}, snippet}}, {channelDetail}) => {
     </Typography>
     </Link>
     <Link to={`/channel/${snippet?.channelId}`}>
-    
-    <Typography variant="subtitle2" fontWeight="bold" color="gray">
+      <Stack direction="row" marginTop="15px">
+      <Avatar alt={snippet?.channelTitle} src={channelDetail?.snippet?.thumbnails?.high?.url} sx={{width:24, height:24}} />
+    <Typography variant="subtitle2" fontWeight="bold" color="gray" marginLeft="5px">
         {snippet?.channelTitle.slice(0,60)}
         <CheckCircle sx={{ fontSize: 14, color:"gray", ml: "5px"}}/>
     </Typography>
+    </Stack>
     </Link>
+
+    
 </CardContent>
     </Card>
 
